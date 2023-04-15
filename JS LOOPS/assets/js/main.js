5b467c5309157cd049a87ff4843df63ccabf60bd
tura1l <127213064+tura1l@users.noreply.github.com>
2023-04-15 11:31:45 +0400

    Add files via upload

---
+++ JS LOOPS/assets/js/main.js
@@ -0,0 +1,25 @@
+// //Task1
+// var num=Number(prompt("Zehmet olmasa eded daxil edin"));
+// var teklik;
+// var sum=0;
+// while (num>0) {
+//     teklik=num%10;
+//     num=num-teklik;
+//     num=num/10;
+//     sum=sum+teklik;
+// }
+// alert(sum)
+
+//Task2
+// var num=Number(prompt("Zehmet olmasa eded daxil edin"));
+// var kvadrat= num*num ;
+//     for(var i=2;i<=kvadrat;i++){
+//      if (num%i==0) {
+//         alert("Murekkedb ededdir");
+//       }  
+//       else{
+//         alert("Sade ededdir");
+//       }
+   
+//     }
+   
